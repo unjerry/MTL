2cd3b9a330ccaa844cf329f38f300555be9555de
unjerry <2351688512@qq.com>
2023-03-23 10:57:18 +0800

    #new file:   product/mathlibrary/include/complexnumber.h #new file:   product/mathlibrary/include/fast_fourier_transform.h #new file:   product/mathlibrary/include/list_convolution.h #new file:   product/mathlibrary/src/complexnumber.cpp #new file:   product/mathlibrary/src/fast_fourier_transform.cpp #new file:   product/mathlibrary/src/list_convolution.cpp

---
+++ product/mathlibrary/include/list_convolution.h
@@ -0,0 +1,9 @@
+#ifndef LIST_CONVOLUTION_H
+#define LIST_CONVOLUTION_H
+
+#include "fast_fourier_transform.h"
+#include <math.h>
+
+std::vector<complexnumber> list_convolution(const std::vector<complexnumber> &a, const std::vector<complexnumber> &b);
+
+#endif
